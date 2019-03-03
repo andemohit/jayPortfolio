b1163cb447c4a637df2794687f4a384d85c91e32
Mohit Ande <andemohit@gmail.com>
2019-03-03 14:18:00 +0530

    Add files via upload

---
+++ src/app/components/experiments/experiments.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiments',
+  templateUrl: './experiments.component.html',
+})
+export class ExperimentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
